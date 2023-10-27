eb293fa62f6807bb1e15d0d7b818f85a909ae0d8
cjpizarroz <90941874+cjpizarroz@users.noreply.github.com>
2023-10-26 21:45:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,36 +12,34 @@ app = FastAPI()
 
 #---------- END POINT NRO 1 --------------
 # ------ usar 'ebi-hime' como dato para consulta
-@app.get("/get_data")
+@app.get("/get_data/docs")
 async def get_data(id):
-    try:
-        dataset = query_data(id)  # Realiza la consulta para obtener el conjunto de datos
-        return JSONResponse(content=dataset)
-    except Exception as e:
-        logging.error(f"Error in read_root: {e}")
-        return {"error": "Internal Server Error"}
-
-def query_data(id):
-    desarrollador = id
-    #
-    columns = ['release_date','item_id', 'developer','genres_Free to Play']
-    df = pd.read_csv('CSV\output_steaam_games.csv', usecols=columns, sep=",", encoding="UTF-8")
-    df_desarrollador = df[df['developer'] == desarrollador]
-
-    # Agrupar los datos por "Año" y contar la cantidad de "item" por año
-    item_por_ano = df_desarrollador.groupby('release_date')['item_id'].nunique().reset_index()
-    df_consulta = item_por_ano
-    df_consulta.rename(columns={'item_id': 'Cantidad de articulos', 'release_date':'Año'}, inplace=True)
-
-    # Agrupar los datos por "Año" y contar la cantidad de "item" por año
-    item_free_to_play = df_desarrollador.groupby('release_date')['genres_Free to Play'].sum().reset_index()
-    df_consulta['Contenido gratis'] = item_free_to_play['genres_Free to Play']/item_por_ano['Cantidad de articulos']
-
-    # Formatear la columna 'Porcentaje' como porcentaje
-    df_consulta['Contenido gratis'] = df_consulta['Contenido gratis'].apply(lambda x: '{:.2%}'.format(x))
-
+    dataset = query_data(id)  # Realiza la consulta para obtener el conjunto de datos
+    return JSONResponse(content=dataset)
     
-    return df_consulta.to_dict(orient='records')
+def query_data(id):
+    try:
+        desarrollador = id
+        
+        columns = ['release_date','item_id', 'developer','genres_Free to Play']
+        df = pd.read_csv('CSV\output_steaam_games.csv', usecols=columns, sep=",", encoding="UTF-8")
+        df_desarrollador = df[df['developer'] == desarrollador]
+
+        # Agrupar los datos por "Año" y contar la cantidad de "item" por año
+        item_por_ano = df_desarrollador.groupby('release_date')['item_id'].nunique().reset_index()
+        df_consulta = item_por_ano
+        df_consulta.rename(columns={'item_id': 'Cantidad de articulos', 'release_date':'Año'}, inplace=True)
+
+        # Agrupar los datos por "Año" y contar la cantidad de "item" por año
+        item_free_to_play = df_desarrollador.groupby('release_date')['genres_Free to Play'].sum().reset_index()
+        df_consulta['Contenido gratis'] = item_free_to_play['genres_Free to Play']/item_por_ano['Cantidad de articulos']
+
+        # Formatear la columna 'Porcentaje' como porcentaje
+        df_consulta['Contenido gratis'] = df_consulta['Contenido gratis'].apply(lambda x: '{:.2%}'.format(x))
+
+        return df_consulta.to_dict(orient='records')
+    except Exception as e:
+        return {"error": (e)}
 
 
 
