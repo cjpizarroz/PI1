b27cc2d046fd0083a1afef00aa83a865cd0eaef6
cjpizarroz <90941874+cjpizarroz@users.noreply.github.com>
2023-10-28 07:23:00 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 #---------- END POINT NRO 1 --------------
 # ------ usar 'ebi-hime' como dato para consulta
 @app.get("/get_data/")
-async def get_data(id):
+async def endpoints_1_Desarrollador(id):
     dataset = query_data(id)  # Realiza la consulta para obtener el conjunto de datos
     return JSONResponse(content=dataset)
     
@@ -53,38 +53,31 @@ def query_data(id):
 
 #---------- END POINT NRO 2 --------------
 # ------ usar 'ebi-hime' como dato para consulta
-@app.get("/get_data_ep1/")
-async def get_data_qp1(id):
+@app.get("/get_data_ep2/")
+async def endpoints_2_User_id(id):
     dataset = query_data_ep2(id)  # Realiza la consulta para obtener el conjunto de datos
     return JSONResponse(content=dataset)
     
 def query_data_ep2(id):
     try:
-        desarrollador = id
-        
-        columns = ['release_date','item_id', 'developer','genres_Free to Play']
-        df = pd.read_csv('CSV//output_steaam_games.csv', usecols=columns, sep=",", encoding="UTF-8")
-        df_desarrollador = df[df['developer'] == desarrollador]
-
-        
-        del df          # libero recursos
-        gc.collect()
-
-        # Agrupar los datos por "Año" y contar la cantidad de "item" por año
-        df_consulta= df_desarrollador.groupby('release_date')['item_id'].nunique().reset_index()
-        df_consulta.rename(columns={'item_id': 'Cantidad de articulos', 'release_date':'Año'}, inplace=True)
-
-        # Agrupar los datos por "Año" y contar la cantidad de "item" por año
-        item_free_to_play = df_desarrollador.groupby('release_date')['genres_Free to Play'].sum().reset_index()
-        df_consulta['Contenido gratis'] = item_free_to_play['genres_Free to Play']/df_consulta['Cantidad de articulos']
-
-        del desarrollador   # libero recursos
-        gc.collect()
-
-        # Formatear la columna 'Porcentaje' como porcentaje
-        df_consulta['Contenido gratis'] = df_consulta['Contenido gratis'].apply(lambda x: '{:.2%}'.format(x))
-
-        return df_consulta.to_dict(orient='records')
+        dataframe = pd.read_csv('CSV\df_ep_2.csv', sep=',', encoding='UTF-8')
+        usuario_data = dataframe[dataframe['user_id'] == id]
+    
+        dinero_gastado = usuario_data['price'].sum()
+    
+        total_juegos = len(usuario_data)
+    
+        porcentaje_recomendacion_negativa = (usuario_data['recommend_False'].sum() / total_juegos) * 100
+        porcentaje_recomendacion_positiva = (usuario_data['recommend_True'].sum() / total_juegos) * 100
+        porcentaje_recomendacion_negativa = '{:.0f} %'.format(porcentaje_recomendacion_negativa)
+        porcentaje_recomendacion_positiva = '{:.0f} %'.format(porcentaje_recomendacion_positiva)
+    
+        return {
+            "dinero_gastado": dinero_gastado,
+            "porcentaje_recomendacion_negativa": porcentaje_recomendacion_negativa,
+            "porcentaje_recomendacion_positiva": porcentaje_recomendacion_positiva,
+            "total_juegos": total_juegos
+        }
         
     except Exception as e:
         return {"error": str(e)}
